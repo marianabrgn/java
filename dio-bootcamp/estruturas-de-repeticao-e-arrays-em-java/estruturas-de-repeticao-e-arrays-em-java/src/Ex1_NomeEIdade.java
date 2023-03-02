642bb9e75e70211b68433773de345f937bf6ee63
marianabrgn <mariana_brandaogoncalves@yahoo.com>
2023-03-01 21:59:04 -0300

    resolução do exercício DIO

--- dio-bootcamp/estruturas-de-repeticao-e-arrays-em-java/estruturas-de-repeticao-e-arrays-em-java/src/Ex1_NomeEIdade.java
+++ dio-bootcamp/estruturas-de-repeticao-e-arrays-em-java/estruturas-de-repeticao-e-arrays-em-java/src/Ex1_NomeEIdade.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Ex1_NomeEIdade {
   public static void main(String[] args) {
 
+    Scanner sc = new Scanner(System.in);
+    int idade;
+    String nome;
+
+    while (true) {
+
+      System.out.println("Digite seu nome:");
+      nome = sc.next();
+      if (nome.equals("0")) {
+        break;
+      }
+      System.out.println("Digite sua idade:");
+      idade = sc.nextInt();
+      if (idade == 0) {
+        break;
+      }
+      System.out.printf("Seu nome é %s, e sua idade é %d. \n", nome, idade);
+    }
   }
 }
