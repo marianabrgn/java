51a3f851ddbe120b0e72c37dc0113edc36527a39
marianabrgn <mariana_brandaogoncalves@yahoo.com>
2023-03-05 15:52:31 -0300

    Resolução do exercício java maior e media - DIO

--- dio-bootcamp/estruturas-de-repeticao-e-arrays-em-java/estruturas-de-repeticao-e-arrays-em-java/src/Ex3_MaiorEMedia.java
+++ dio-bootcamp/estruturas-de-repeticao-e-arrays-em-java/estruturas-de-repeticao-e-arrays-em-java/src/Ex3_MaiorEMedia.java
@@ -5,12 +5,18 @@ public class Ex3_MaiorEMedia {
     Scanner sc = new Scanner(System.in);
 
     float[] numeros = new float[5];
-    float media, soma;
+    float media, soma = 0, maior = 0;
 
     for (int i = 0; i < numeros.length; i++) {
       System.out.println("Digite um número:");
       numeros[i] = sc.nextFloat();
+      soma += numeros[i];
+      if (maior < numeros[i]) {
+        maior = numeros[i];
+      }
     }
+    media = soma / 5;
 
+    System.out.printf("O maior número é %.2f e a media dos números é %.2f", maior, media);
   }
 }
