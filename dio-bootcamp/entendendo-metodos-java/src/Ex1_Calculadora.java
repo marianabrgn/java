d6335e5630c17350b9d6cc61ef53f1415f6cbe2e
marianabrgn <mariana_brandaogoncalves@yahoo.com>
2023-03-13 09:02:25 -0300

    (exercício 1 - calculadora bootcamp DIO)

---
+++ dio-bootcamp/entendendo-metodos-java/src/Ex1_Calculadora.java
@@ -0,0 +1,23 @@
+public class Ex1_Calculadora {
+
+  public static void main(String[] args) {
+
+  }
+
+  public float somar(float numero1, float numero2) {
+    return numero1 + numero2;
+  }
+
+  public float subtrair(float numero1, float numero2) {
+    return numero1 - numero2;
+  }
+
+  public float multiplicar(float numero1, float numero2) {
+    return numero1 * numero2;
+  }
+
+  public float dividir(float numero1, float numero2) {
+    return numero1 / numero2;
+  }
+
+}
