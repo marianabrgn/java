89ff5fc4c94a34a83883e6b3da96bfccfbd3432a
marianabrgn <mariana_brandaogoncalves@yahoo.com>
2023-03-14 08:32:36 -0300

    Resolução do exercício calculadora em java - bootcamp DIO

--- dio-bootcamp/entendendo-metodos-java/src/Ex1_Calculadora.java
+++ dio-bootcamp/entendendo-metodos-java/src/Ex1_Calculadora.java
@@ -1,22 +1,18 @@
 public class Ex1_Calculadora {
 
-  public static void main(String[] args) {
-
-  }
-
-  public float somar(float numero1, float numero2) {
+  public static double somar(double numero1, double numero2) {
     return numero1 + numero2;
   }
 
-  public float subtrair(float numero1, float numero2) {
+  public static double subtrair(double numero1, double numero2) {
     return numero1 - numero2;
   }
 
-  public float multiplicar(float numero1, float numero2) {
+  public static double multiplicar(double numero1, double numero2) {
     return numero1 * numero2;
   }
 
-  public float dividir(float numero1, float numero2) {
+  public static double dividir(double numero1, double numero2) {
     return numero1 / numero2;
   }
 
